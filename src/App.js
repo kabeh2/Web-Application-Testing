06b4b60107d1896e50b5c3302fe7d5c0a4f9bb40
kabeh2 <mattcnk@hotmail.com>
2019-09-18 19:53:51 -0400

    Updated logic for scoreboard display

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ class App extends Component {
     homerun: 0,
     guestScore: 0,
     homeScore: 0,
-    guestAtBat: false,
-    homeAtBat: false,
-    btnLabel: "Play Ball!"
+    guestAtBat: true,
+    homeAtBat: false
   };
 
   resetPosession = () => {
@@ -21,33 +20,96 @@ class App extends Component {
       strike: 0
     });
   };
+  resetAll = () => {
+    this.setState({
+      ball: 0,
+      strike: 0,
+      outs: 0,
+      guestAtBat: !this.state.guestAtBat,
+      homeAtBat: !this.state.homeAtBat
+    });
+  };
+
+  nextPossession = () => {
+    if (this.state.outs === 3) {
+      alert("Next Team at Bat!");
+      this.resetAll();
+    } else {
+      this.setState({
+        outs: this.state.outs + 1
+      });
+      alert("Out!");
+
+      this.resetPosession();
+    }
+  };
 
   handleClick = () => {
+    const {
+      strike,
+      ball,
+      outs,
+      guestAtBat,
+      guestScore,
+      homeAtBat,
+      homeScore
+    } = this.state;
+
     const play = Math.random() * 100;
     console.log(play);
 
-    if (this.state.strike > 2 || this.state.ball > 3) {
-      this.resetPosession();
-      this.setState({
-        btnLabel: "Click for Next Possession"
-      });
-    } else if (this.state.strike < 3) {
-      if (play < 33) {
+    /////////////////////////////////////
+    /////////////////////////// IF STRIKE
+    if (play < 45) {
+      if (strike < 3 && outs < 3 && ball < 4) {
         this.setState({
-          strike: this.state.strike + 1,
-          btnLabel: "Play Ball!"
+          strike: strike + 1
         });
-      } else if (play > 33 && play < 66) {
+        alert("Strike!");
+      } else if (strike > 2 && outs < 3 && ball < 4) {
         this.setState({
-          ball: this.state.ball + 1,
-          btnLabel: "Play Ball!"
+          outs: outs + 1
         });
-      } else {
+        alert("Out!");
+        this.resetPosession();
+      }
+    }
+    /////////////////////////////////////
+    /////////////////////////// IF BALL
+    else if (play > 45 && play < 90) {
+      if (ball < 4 && outs < 3 && strike < 3) {
         this.setState({
-          guestScore: this.state.guestScore + 1,
-          homeScore: this.state.homeScore + 1,
-          btnLabel: "Play Ball!"
+          ball: ball + 1
         });
+        alert("Ball!");
+      } else if (ball > 3 && outs < 3 && strike < 3) {
+        this.setState({
+          ball: ball + 1,
+          outs: outs + 1
+        });
+        alert("Out!");
+        this.resetPosession();
+      }
+    }
+    /////////////////////////////////////
+    ////////////////////////// IF HOMERUN
+    else if (play > 90) {
+      if (guestAtBat) {
+        if (strike < 3 && ball < 4) {
+          this.setState({
+            guestScore: guestScore + 1
+          });
+          alert("Guest Team Homerun!");
+          this.resetPosession();
+        }
+      } else if (homeAtBat) {
+        if (strike < 3 && ball < 4) {
+          this.setState({
+            homeScore: homeScore + 1
+          });
+          alert("Home Team Homerun!");
+          this.resetPosession();
+        }
       }
     }
   };
@@ -66,7 +128,19 @@ class App extends Component {
 
     return (
       <div className="App">
-        <button onClick={this.handleClick}>{this.state.btnLabel}</button>
+        {ball === 4 || strike === 3 ? (
+          <button onClick={this.nextPossession}>
+            Click for Next Possession!
+          </button>
+        ) : outs === 3 ? (
+          <button onClick={this.nextPossession}>Click for Next TEAM!</button>
+        ) : (
+          <button onClick={this.handleClick}>Play Ball!</button>
+        )}
+        <div className="at-bat" style={{ color: "green" }}>
+          At Bat: {guestAtBat ? "Guest" : "Home"}
+        </div>
+
         <Display
           ball={ball}
           strike={strike}
@@ -83,3 +157,44 @@ class App extends Component {
 }
 
 export default App;
+
+// if (this.state.outs < 3) {
+//   if (this.state.strike > 2 || this.state.ball > 3) {
+//     this.resetPosession();
+//     this.setState({
+//       outs: this.state.outs + 1
+//     });
+//   } else if (this.state.strike < 3 || this.state.ball < 4) {
+//     if (play < 33) {
+//       this.setState({
+//         strike: this.state.strike + 1
+//       });
+//       alert("Strike!");
+//     } else if (play > 33 && play < 66) {
+//       this.setState({
+//         ball: this.state.ball + 1
+//       });
+//       alert("Ball!");
+//     } else {
+//       this.setState({
+//         guestScore: this.state.guestScore + 1,
+//         homeScore: this.state.homeScore + 1
+//       });
+//       alert("Homerun!");
+//       this.resetPosession();
+//     }
+//   }
+// } else {
+//   this.setState({
+//     outs: 0,
+//     guestAtBat: !this.state.guestAtBat,
+//     homeAtBat: !this.state.homeAtBat
+//   });
+// }
+
+// if (this.state.strike > 2 || this.state.ball > 3) {
+//   this.resetPosession();
+//   this.setState({
+//     outs: this.state.outs + 1
+//   });
+// }
